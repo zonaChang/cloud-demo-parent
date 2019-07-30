cdafcdf970010e7de196e9d5c3be7ad6f5ebb422
changerzhuo <cez@prowizdom.com>
2019-07-30 08:57:27 +0800

    init

---
+++ eureka-register-center/src/main/java/com/carl/study/cloud/eurekaregistercenter/EurekaRegisterCenterApplication.java
@@ -0,0 +1,15 @@
+package com.carl.study.cloud.eurekaregistercenter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRegisterCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaRegisterCenterApplication.class, args);
+        System.out.println(StringUtils.isBlank(""));
+    }
+
+}
---
+++ eureka-register-center/src/test/java/com/carl/study/cloud/eurekaregistercenter/EurekaRegisterCenterApplicationTests.java
@@ -0,0 +1,16 @@
+//package com.carl.study.cloud.eurekaregistercenter;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class EurekaRegisterCenterApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
